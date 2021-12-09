dce2b5636f304f94ff5ebc3345b0872eca94e07f
Pablo Lario Gómez <62502964+Thelario@users.noreply.github.com>
2021-12-09 15:29:47 +0100

    Documenting Window Creation

--- Satellite Engine/Satellite Engine/Window.cpp
+++ Satellite Engine/Satellite Engine/Window.cpp
@@ -19,18 +19,26 @@ namespace Satellite
 
 		Window::~Window()
 		{
+			// When you are done using GLFW, typically just before the application exits, you need to terminate GLFW.
+			// This destroys any remaining windows and releases any other resources allocated by GLFW. After this call, 
+			// you must initialize GLFW again before using any GLFW functions that require it.
 			glfwTerminate();
 		}
 
 		bool Window::init()
 		{
+			// We try to initialize GLFW
 			if (!glfwInit())
 			{
 				std::cout << "Failed to initialize GLFW!" << std::endl;
 				return false;
 			}
 
+			// This function creates a window and its associated OpenGL or OpenGL ES context.
 			_window = glfwCreateWindow(_width, _height, _title, NULL, NULL);
+
+			// When a window and context is no longer needed, destroy it:
+			// glfwDestroyWindow(_window);
 
 			if (!_window)
 			{
@@ -39,20 +47,37 @@ namespace Satellite
 				return false;
 			}
 
+			// Before I can use the OpenGL API, you must have a current OpenGL context.
 			glfwMakeContextCurrent(_window);
+
+			// Callback when resizing the window happens.
 			glfwSetWindowSizeCallback(_window, windowResize);
 			return true;
 		}
 
 		void Window::clear() const
 		{
+			// glClear clears the buffers to preset the values.
+			// GL_COLOR_BUFFER_BIT: indicates the buffers currently enabled for color writing.
+			// GL_DEPTH_BUFFER_BIT: indicates the depth buffer.
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		}
 
 		void Window::update()
 		{
+			// GLFW needs to communicate regularly with the window system both in order to receive events and to show 
+			// that the application hasn't locked up. Event processing must be done regularly while you have visible 
+			// windows and is normally done each frame after buffer swapping.
 			glfwPollEvents();
-			//glfwGetFramebufferSize(_window, &_width, &_height);
+
+			// This function retrieves the size, in pixels, of the framebuffer of the specified window. If you wish 
+			// to retrieve the size of the window in screen coordinates, see glfwGetWindowSize.
+			// glfwGetFramebufferSize(_window, &_width, &_height);
+
+			// GLFW windows by default use double buffering. That means that each window has two rendering buffers; 
+			// a front buffer and a back buffer. The front buffer is the one being displayed and the back buffer the 
+			// one you render to. When the entire frame has been rendered, the buffers need to be swapped with one another,
+			// so the back buffer becomes the front bufferand vice versa.
 			glfwSwapBuffers(_window);
 		}
 
--- Satellite Engine/Satellite Engine/main.cpp
+++ Satellite Engine/Satellite Engine/main.cpp
@@ -4,14 +4,20 @@
 
 int main()
 {
+	// We say that we want to use the code in the Satellite and Graphics namespaces.
 	using namespace Satellite;
 	using namespace Graphics;
 
+	// We create a new Window object, passing the name, width and height.
 	Window window("Satellite", 1080, 720);
+
+	// glClearColor specify clear values for the color buffers (from what I can see, it changes the background color of the window).
 	glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
 
+	// We loop infinitely till the user closes the window (with the red button in the right upper corner).
 	while (!window.closed())
 	{
+		// We print in the console the width and height of the window we have created.
 		std::cout << window.getWidth() << ", " << window.getHeight() << std::endl;
 
 		window.clear();
